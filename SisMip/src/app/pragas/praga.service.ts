e213d80ef05cff001c9f46ff2c2030e72b0ad011
Thales Neves <thales.neves@prs.com.br>
2020-07-04 17:58:14 -0300

    teste de commit

--- SisMip/src/app/pragas/praga.service.ts
+++ SisMip/src/app/pragas/praga.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import { PragaModel } from '../models/praga.model';
+import { PragaModel } from 'src/app/models/praga.model';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
